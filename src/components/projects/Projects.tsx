546b16bf5b13c87de088d98ea6e70aebe074cb85
João Mota <47133204+jppm99@users.noreply.github.com>
2021-08-06 03:37:34 +0100

    Added new projects and updated carousel

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,13 +1,17 @@
 import { Component } from 'react';
-import ScrollMenu from 'react-horizontal-scrolling-menu';
+import Splide from '@splidejs/splide';
+
 
 import './Projects.css';
+import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+
 
 import ldso from "../../assets/ldso.jpg";
 import personalWebsite from "../../assets/personalWebsite.jpg";
 import ai from "../../assets/iart.jpg";
 import hyperCasual from "../../assets/hypercasual.jpg";
 import fileTransfer from "../../assets/fileTransfer.jpg";
+import lostForest from "../../assets/lostForest.jpg";
 
 const projects = [
     {
@@ -29,6 +33,31 @@ const projects = [
         ]
     },
     {
+        image_path: hyperCasual,
+        title: "Hyper-casual mobile game",
+        description: `
+                An hyper-casual mobile game developed in Unity called Falling Colors. Available on both
+                Google Play and the App Store!
+            `,
+        redirect: "https://apps.apple.com/us/app/falling-colors-catch-them/id1561799594",
+        technologies: [
+            "fab fa-unity",
+            "fab fa-app-store-ios"
+        ]
+    },
+    {
+        image_path: lostForest,
+        title: "Open world survival game",
+        description: `
+                Lost Forest is a horror-survival game developed in Unity.
+                Available on itch.io!
+            `,
+        redirect: "https://jppm99.itch.io/lost-forest",
+        technologies: [
+            "fab fa-unity",
+        ]
+    },
+    {
         image_path: ai,
         title: "Predicting the odds for football matches",
         description: `
@@ -38,18 +67,6 @@ const projects = [
         redirect: "https://github.com/jppm99/IART/blob/master/T2/src/notebook.ipynb",
         technologies: [
             "fab fa-python",
-        ]
-    },
-    {
-        image_path: hyperCasual,
-        title: "Hyper-casual mobile game",
-        description: `
-                An hyper-casual mobile game developed in Unity that I
-                am working on. (work in progress)
-            `,
-        redirect: "",
-        technologies: [
-            "fab fa-unity",
         ]
     },
     {
@@ -97,10 +114,13 @@ class Projects extends Component {
     get_items = () =>
         projects.map((element, index) => {
             return (
-                <div key={index} className="menu-item m-5">
+                <li key={index}
+                    className={"menu-item m-5 splide__slide" + ((element.redirect != null && element.redirect !== "") ? " clickable" : "")}
+                    onClick={() => this.on_proj_click(index)
+                    }>
                     <div className="text-center internal my-0 h-100">
                         <div className="top">
-                            <img src={ element.image_path } alt="Project's" className="" />
+                            <img src={ element.image_path } alt="Project's" className="img-fluid"/>
                         </div>
                         <div className="middle p-3 text-left">
                             <h4><strong>{element.title}</strong></h4>
@@ -111,30 +131,18 @@ class Projects extends Component {
                                 element.technologies.map((tech) => {
                                     return (
                                         <p className={"tech pl-2 " + tech} />
-                                    )
-                                })
-                            }
+                                        )
+                                    })
+                                }
                         </div>
                     </div>
-                </div>
+                </li>
             )
         }
     );
-
-    arrow_left = (
-        <div className="arrow-prev">
-            {'<'}
-        </div>
-    );
     
-    arrow_right = (
-        <div className="arrow-next">
-            {'>'}
-        </div>
-    );
-
-    on_proj_click(item_key: string | number | null) {
-        if (item_key && item_key !== "") {
+    on_proj_click(item_key: string | number | null) {        
+        if (item_key != null && item_key !== "") {
             let item: any = projects[+item_key];
             if (item.redirect !== "") {
                 window.open(item.redirect, '_blank')?.focus();
@@ -142,13 +150,28 @@ class Projects extends Component {
         }
     }
 
+    componentDidMount() {
+        new Splide( '#splide', {
+            type: 'loop',
+            perPage: 3,
+            perMove: 1,
+            pagination: false,
+            padding: {
+                right: '5rem',
+                left : '5rem',
+            },
+        })
+            .mount();
+    }
+
     render() {
         return (
             <div className="projects-background py-5">
                 <div className="Projects container d-flex flex-column align-items-center justify-content-center py-5" id="Projects">
                     <h2>Some <strong>projects</strong> I have worked on:</h2>
+
                     <div className="mt-4 projects-holder">
-                        <ScrollMenu
+                        {/* <ScrollMenu
                             data={this.menuItems}
                             arrowLeft={this.arrow_left}
                             arrowRight={this.arrow_right}
@@ -158,7 +181,14 @@ class Projects extends Component {
                             translate={1}
                             onSelect={this.on_proj_click}
                             itemClass={'align-top'} //! don't remove
-                        />
+                        /> */}
+                        <div id="splide" className="splide">
+                            <div className="splide__track">
+                                <ul className="splide__list">
+                                    {this.menuItems}
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
